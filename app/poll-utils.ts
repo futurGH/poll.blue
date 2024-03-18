05e4c2b66a1170cdcd46c54ba18628725f6581d3
ShreyanJain9 <shreyan.jain.9@outlook.com>
2024-03-17 17:34:22 -0700

    what the actual

--- app/poll-utils.ts
+++ app/poll-utils.ts
@@ -40,7 +40,7 @@ interface PollPost {
     pollFacets: AppBskyRichtextFacet.Main[];
 }
 
-function resolveHandle(handle: string) {
+async function resolveHandle(handle: string) {
     try {
         const response = await fetch(`https://bsky.social/xrpc/com.atproto.identity.resolveHandle?handle=${handle}`);
         const data = await response.json();
@@ -64,7 +64,7 @@ export function generatePollText(options: GenerationOptions): PollPost {
             { text: `${poll.question}\n\n`, link: undefined, truncate: 'no' },
         ];
     } else {
-        const author_did = resolveHandle(author);
+        const author_did = resolveHandle(author).then((did) => did);
         postTemplate = [
             { text: `"${poll.question}"`, link: undefined, truncate: 'no', pollFacet: 'blue.poll.post.facet#question' },
             { text: ` asked by `, link: undefined, truncate: 'yes' },
